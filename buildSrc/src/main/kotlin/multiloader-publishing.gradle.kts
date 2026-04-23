d2a64c398092b764dd0780c3c7a6a5295afbdc26
ChampionAsh5357 <ash@ashwork.net>
2026-04-22 20:42:22 -0400

    fix(cicd,push): Remove repsy output

--- buildSrc/src/main/kotlin/multiloader-publishing.gradle.kts
+++ buildSrc/src/main/kotlin/multiloader-publishing.gradle.kts
@@ -43,14 +43,6 @@ publishing {
                 password = System.getenv("GITHUB_TOKEN")
             }
         }
-        maven {
-            name = "Repsy"
-            url = uri("https://repo.repsy.io/ashwork/mc-mods")
-            credentials {
-                username = System.getenv("REPSY_USERNAME")
-                password = System.getenv("REPSY_PASSWORD")
-            }
-        }
     }
 
     publications.withType<MavenPublication>().configureEach {
